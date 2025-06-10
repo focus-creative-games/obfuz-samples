cd5ab993ca8afa9a01b882f1cda2a78b52e9cf4d
walon <walon@code-philosophy.com>
2025-06-10 14:03:52 +0800

    添加WorkWithXLua项目

---
+++ WorkWithXLua/Assets/Bootstrap.cs
@@ -0,0 +1,39 @@
+using JetBrains.Annotations;
+using Obfuz;
+using Obfuz.EncryptionVM;
+using System.Collections;
+using System.Collections.Generic;
+using Tutorial;
+using UnityEngine;
+
+
+public class Bootstrap : MonoBehaviour
+{
+    // 初始化EncryptionService后被混淆的代码才能正常运行，
+    // 因此尽可能地早地初始化它。
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    private static void SetUpStaticSecretKey()
+    {
+        //Debug.Log("SetUpStaticSecret begin");
+        EncryptionService<DefaultStaticEncryptionScope>.Encryptor = new GeneratedEncryptionVirtualMachine(Resources.Load<TextAsset>("Obfuz/defaultStaticSecretKey").bytes);
+        //Debug.Log("SetUpStaticSecret end");
+    }
+
+    private void Awake()
+    {
+        ObfuscationInstincts.RegisterReflectionType<BaseClass>();
+        ObfuscationInstincts.RegisterReflectionType<DerivedClass>();
+        ObfuscationInstincts.RegisterReflectionType<Tutorial.ICalc>();
+        ObfuscationInstincts.RegisterReflectionType<DerivedClass.InnerCalc>();
+        ObfuscationInstincts.RegisterReflectionType<DerivedClass.TestEnumInner>();
+        ObfuscationInstincts.RegisterReflectionType<Tutorial.TestEnum>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, Obfuz");
+        //int a = Add(10, 20);
+        //Debug.Log($"a = {a}");
+    }
+}
